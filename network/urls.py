95c2936457bda9112b9d3c525c299ee0c6971ca0
Phan Anh <phananh.183@gmail.com>
2021-08-07 11:42:06 +0800

    Add code for following tab

--- network/urls.py
+++ network/urls.py
@@ -8,10 +8,12 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("user/<int:profile_id>", views.profile, name='profile'),
-
+    path("posts/following", views.following, name='following'),
+    
     # API routes
     path("posts", views.showPost, name="posts"),
     path("new-post", views.post, name="newPost"),
     path("posts/<int:profile_id>", views.showPostByProfile, name='show-post-by-profile'),
-    path("user/<int:profile_id>/toggleFollow", views.toggleFollow, name='toggle-follow')
+    path("user/<int:profile_id>/toggleFollow", views.toggleFollow, name='toggle-follow'),
+    path("following", views.followingData, name="following-data"),
 ]
